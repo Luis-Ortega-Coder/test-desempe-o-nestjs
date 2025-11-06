dd78d140e0d73e94cd750d24a921868f6325dc07
walex0627 <walteralex0627@gmail.com>
2025-11-06 16:24:54 -0500

    feat(config/, app.module and main.ts): Create the connection to database and configuration of swagger

--- app-nest/src/app.module.ts
+++ app-nest/src/app.module.ts
@@ -1,9 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// Determine if running inside Docker container
+const runningInDocker = process.env.RUNNING_IN_DOCKER === 'true';
+
+//Resolve path to external .env file
+const externalEnvPath = join(__dirname, '..', '..', '.env');
 @Module({
-  imports: [],
+  imports: [
+    //Global configuration module
+    ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+    ignoreEnvFile: runningInDocker,
+    envFilePath: runningInDocker ? undefined : externalEnvPath,
+  }),
+
+    //Connection to Postgres database
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get<string>('database.user'),
+        password: config.get<string>('database.password'),
+        database: config.get<string>('database.db'),
+        autoLoadEntities: true,
+        synchronize: true, 
+      })
+    })
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
--- app-nest/src/main.ts
+++ app-nest/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //Swagger set up
+  const config = new DocumentBuilder()
+  .setTitle('E-commerce API')
+  .setDescription('API documentation for the E-commerce application')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Try to connect to the database to verify connection
+  try {
+    const dataSource = app.get(DataSource);
+    await dataSource.query('SELECT 1');
+    console.log('✅ Database connection established successfully');
+  } catch (err) {
+    console.error('❌ Database connection failed:', err);
+  }
+
+  const port = process.env.APP_PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server running on port ${port}`)
+  console.log(`📘 Swagger docs available at http://localhost:${port}/api/docs`);
 }
+
 bootstrap();
